ce17f71de4a91f824b7bfbadbc2690fd60ef2dd7
Ivan Osuna <ivan.osuna.ayuste@gmail.com>
2024-01-04 14:00:45 +0100

    dia 2

--- projectos/SparkCore/src/main/java/com/curso/ContarHashtags.java
+++ projectos/SparkCore/src/main/java/com/curso/ContarHashtags.java
@@ -3,6 +3,7 @@ package com.curso;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import scala.Tuple2;
 
 import java.io.IOException;
 import java.io.Serializable;
@@ -39,12 +40,34 @@ public class ContarHashtags {
                 .filter( palabra -> palabra.startsWith("#") )           // Me quedo solo con los hashtags                                Stream<String>
                 .map( String::toLowerCase )                                    // Paso todos los hashtags a minúsculas                          Stream<String>
                 .map( hashtag -> hashtag.substring(1) )             // Elimino el # de cada hashtag                                  Stream<String>
-                .filter( hashtag -> getPalabrasProhibidas().stream().noneMatch( hashtag::contains ) );  // Eliminar los hashtags que contengan palabras prohibidas         Stream<String>
+                .filter( hashtag -> getPalabrasProhibidas().stream().noneMatch( hashtag::contains ) )  // Eliminar los hashtags que contengan palabras prohibidas         Stream<String>
+
+        /*
+
+        En un java.util.Map, las claves (KEYS) son únicos a nivel de la colección. En un JavaPairRDD<K,V> Los keys no se tratan como elementos UNICOS
+        new Tuple2<>(dato1, dato2) -> MAP ->  JavaRDD<Tuple2<String, Integer>>
+                        ^    ^
+                    palabra , 1
+        En Spark existe un tipo especial de RDD: PairRDD<String, Integer> -> RDD<Tuple2<String, Integer>>
+        Existe una función para ello, llamada mapToPair -> reduceByKey(sume los valores al juntar las entradas con el mismo Key)
+                    goodVives 17
+                    odioLasNavidades 3
+                    enLaPlaya  14
+        Tengo que ordenar... y me tengo que apañar con el sortByKey... es lo que hay! Que me toca entonces? Dar la vuelta a la tupla... otro map
+        Una vez ordenado, extraigo los N primeros... Y TACHAN !!!! TRENDING TOPICS CALCULADOS!
+
+        De una tupla, puedo sacar el primer valor con ._1... y el segundo valor con ._2
+        Para al final sacar por pantalla el resultado
+        */
+                .mapToPair( hashtag -> new Tuple2<>(hashtag, 1))
+                .reduceByKey(Integer::sum)
+                .mapToPair(tupla -> new Tuple2<>(tupla._2, tupla._1))
+                .sortByKey(false)
+                .take(10);
 
         // PASO 4: Procesar el resultado: (guardarlo en una BBDD, fichero..., imprimirlo por pantalla)
         hashtags
-                .collect()
-                .forEach( System.out::println );
+                .forEach( tupla -> System.out.println("Hashtag: "+ tupla._2+", cantidad: "+tupla._1) );
 
         // PASO 5: Cerrar conexión con el cluster
         conexion.close();

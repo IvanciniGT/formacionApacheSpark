5dbc5053e86d991ca1e6ab0039fb7f87a254c626
Ivan Osuna <ivan.osuna.ayuste@gmail.com>
2024-01-04 17:08:18 +0100

    dia 2

--- projectos/SparkCore/src/main/java/com/curso/Persona.java
+++ projectos/SparkCore/src/main/java/com/curso/Persona.java
@@ -1,19 +1,54 @@
 package com.curso;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
-@Data
+import java.util.Optional;
+
+
+@EqualsAndHashCode
+@ToString
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 public class Persona {
-    private String nombre;
-    private String apellidos;
-    private long edad;
-    private String cp;
-    private String email;
-    private String dni;
+    @Getter @Setter private String nombre;
+    @Getter @Setter private String apellidos;
+    @Getter @Setter private long edad;
+    @Getter @Setter private String cp;
+    @Getter @Setter private String email;
+    @Getter private String dni;
+    private Integer numeroDni;
+    private String letraDni;
+    private boolean dniValido;
+
+    public Optional<Integer> getNumeroDni(){
+        return Optional.ofNullable(numeroDni);
+    }
+    public Optional<String> getLetraDni(){
+        return Optional.ofNullable(letraDni);
+    }
+
+    public void setDni(String dni){
+        this.dni = dni;
+        validarDni(dni);
+    }
+
+    private static final String LETRAS_DNI = "TRWAGMYFPDXBNJZSQVHLCKE";
+
+    private void validarDni(String dni){
+        // Debe validar el dni
+        // Y rellenar las variables:
+        // - numeroDni
+        // - letraDni
+        // - dniValido
+    }
+
+    public Optional<String> normalizarDni(boolean puntosDeMiles, boolean cerosIzquierda, String separadorLetra){
+        if(!dniValido){
+            return Optional.empty();
+        }
+        String dniNormalizado = "";
+        // Aqui haré la normalización
+        return Optional.of(dniNormalizado);
+    }
 }
